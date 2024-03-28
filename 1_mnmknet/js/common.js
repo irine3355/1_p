edf5d33512b660a3546a17969cdfd5dc74b8c7e0
irine3355 <irine3355@gmail.com>
2024-03-28 12:51:03 +0900

    메인페이지

---
+++ 1_mnmknet/js/common.js
@@ -0,0 +1,15 @@
+// Initialize Swiper 
+        window.addEventListener("DOMContentLoaded", function()) {
+
+        }   
+                    const swiper = new Swiper(".mySwiper", {
+                    navigation: {
+                        nextEl: ".swiper-button-next",
+                        prevEl: ".swiper-button-prev",
+                    },
+                    pagination: {
+                        el: ".swiper-pagination",
+                      },
+                    }); 
+             });
+                    
